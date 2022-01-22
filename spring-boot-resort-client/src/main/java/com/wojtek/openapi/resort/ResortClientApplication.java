eb00a4f089f225abde5588b89a7a5df8a4aa91d8
wmaliga <wojciech.maliga@gmail.com>
2022-01-22 19:22:59 +0100

    Spring Boot Application

---
+++ spring-boot-resort-client/src/main/java/com/wojtek/openapi/resort/ResortClientApplication.java
@@ -0,0 +1,12 @@
+package com.wojtek.openapi.resort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResortClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResortClientApplication.class, args);
+    }
+}
---
+++ spring-boot-resort-service/src/main/java/com/wojtek/openapi/resort/ResortServiceApplication.java
@@ -0,0 +1,12 @@
+package com.wojtek.openapi.resort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResortServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResortServiceApplication.class, args);
+    }
+}
